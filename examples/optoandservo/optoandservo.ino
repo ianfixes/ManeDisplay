d2dbae459292b263700c00f924be3fd09cfa45ba
Ian Katz <ianfixes@gmail.com>
2023-02-15 23:46:44 -0500

    Rewrite optoandservo using CalibratedServo

--- examples/optoandservo/optoandservo.ino
+++ examples/optoandservo/optoandservo.ino
@@ -1,31 +1,25 @@
 #include <Servo.h>
 #include <SlaveProperties.h>
+#include <DashState.h>
 
-Servo fuelgauge;  // create servo object to control a servo
-Servo tempgauge;
-Servo oilgauge;
+// the pin, input min/max and output min/max are now part of the initialization
+CalibratedServo fuelgauge(SlavePin::Values::fuelServo, 0, 1023, 0, 180);
+CalibratedServo tempgauge(SlavePin::Values::tempServo, 0, 1023, 0, 180);
+CalibratedServo oilgauge( SlavePin::Values::oilServo,  0, 1023, 0, 180);
 
 void setup() {
-  fuelgauge.attach(SlavePin::Values::fuelServo);  // attaches the fuel gauge servo to pin 6
-  tempgauge.attach(SlavePin::Values::tempServo);  // attaches the temp gauge servo to pin 5
-  oilgauge.attach( SlavePin::Values::oilServo);   // attaches the oil gauge servo to pin 3
+  fuelgauge.setup();
+  tempgauge.setup();
+  oilgauge.setup();
 
   pinMode(SlavePin::Values::optoCoupler, OUTPUT);
   pinMode(SlavePin::Values::ignitionInput, INPUT);
 }
 
 void loop() {
-  const int fuelSignal = analogRead(SlavePin::Values::fuelInput);   // reads the value of the potentiometer (value between 0 and 1023)
-  const int fuelPosition = map(fuelSignal, 0, 1023, 0, 180);        // scale it to use it with the servo (value between 0 and 180)
-  fuelgauge.write(fuelPosition);                            // sets the servo position according to the scaled value
-
-  const int tempSignal = analogRead(SlavePin::Values::temperatureInput);  // reads the value of the potentiometer (value between 0 and 1023)
-  const int tempPosition = map(tempSignal, 0, 1023, 0, 180);      // scale it to use it with the servo (value between 0 and 180)
-  tempgauge.write(tempPosition);                                  // sets the servo position according to the scaled value
-
-  const int oilSignal = analogRead(SlavePin::Values::oilInput);  // reads the value of the potentiometer (value between 0 and 1023)
-  const int oilPosition = map(oilSignal, 0, 1023, 0, 180);       // scale it to use it with the servo (value between 0 and 180)
-  oilgauge.write(oilPosition);                           // sets the servo position according to the scaled value
+  fuelgauge.write(analogRead(SlavePin::Values::fuelInput));
+  tempgauge.write(analogRead(SlavePin::Values::temperatureInput));
+  oilgauge.write(analogRead(SlavePin::Values::oilInput));
 
   // set the optocoupler to be the opposite of the ignition state
   const int useOpto = digitalRead(SlavePin::Values::ignitionInput) == LOW;
